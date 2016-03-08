56676281c790183d88fb0479b871026b11e623ae
vetoshko <vetoshkodasha@gmail.com>
2016-03-09 00:21:44 +0500

    Наверное, *

--- multivarka.js
+++ multivarka.js
@@ -1,65 +1,119 @@
 const MongoClient = require('mongodb').MongoClient;
 
+function addCondition(object, goodOperator, badOperator, value) {
+    var conditionParameter = {};
+    if (object.denial) {
+        conditionParameter[badOperator] = value;
+        object.request[object.conditionWord] = conditionParameter;
+        object.denial = false;
+    } else {
+        conditionParameter[goodOperator] = value;
+        object.request[object.conditionWord] = conditionParameter;
+    }
+    return object;
+};
+
+function connectToMongo(obj, callback) {
+    MongoClient.connect(obj.url, function (err, db) {
+        if (err) {
+            callback(err);
+        } else {
+            var currentCollection = db.collection(obj.collectionName.toString());
+            callback(null, currentCollection, db, obj.request);
+        }
+    });
+};
+
 module.exports = {
     server: function (uri) {
         this.url = uri;
         this.request = {};
-        this.not = false;
+        this.denial = false;
+        this.setData = {};
         return this;
     },
     collection: function (collectionName) {
-        this.collection = collectionName;
+        this.collectionName = collectionName;
+        return this;
+    },
+    not: function () {
+        this.denial = true;
         return this;
     },
     where: function (conditionWord) {
         this.conditionWord = conditionWord;
         return this;
     },
-    equal: function (value) {
-        if (this.not) {
-            this.request[this.conditionWord] = {$ne: value};
-            this.not = false;
-        } else {
-            this.request[this.conditionWord] = {$eq: value};
-        };
+    set: function functionName(field, key) {
+        this.set[field] = key;
         return this;
     },
+    equal: function (value) {
+        return addCondition(this, '$eq', '$gte', value);
+    },
     lessThan: function (value) {
-        if (this.not) {
-            this.request[this.conditionWord] = {$gte: value};
-            this.not = false;
-        } else {
-            this.request[this.conditionWord] = {$lt: value};
-        };
+        return addCondition(this, '$lt', '$ne', value);
     },
     greatThan: function (value) {
-        if (this.not) {
-            this.request[this.conditionWord] = {$lte: value};
-            this.not = false;
-        } else {
-            this.request[this.conditionWord] = {$ge: value};
-        };
+        return addCondition(this, '$ge', '$lte', value);
     },
     include: function (values) {
-        if (this.not) {
-            this.request[this.conditionWord] = {$nin: value};
-            this.not = false;
-        } else {
-            this.request[this.conditionWord] = {$in: value};
-        };
+        return addCondition(this, '$in', '$nin', values);
     },
-    find: function (callback) {
-        var obj = this;
-        MongoClient.connect(this.url, function (err, db) {
+    remove: function(callback) {
+        connectToMongo(this, function (err, collection, db, condition) {
             if (err) {
                 callback(err);
             } else {
-                var collection = db.collection(obj.collection.toString());
-                collection.find(obj.request).toArray(function (err, docs) {
+                collection.deleteMany(condition, function (err, docs) {
                     callback(err, docs);
                     db.close();
+                    this.request = {};
                 });
-            };
+            }
+        });
+    },
+    find: function (callback) {
+        connectToMongo(this, function (err, collection, db, condition) {
+            if (err) {
+                callback(err);
+            } else {
+                collection.find(condition).toArray(function (err, docs) {
+                    callback(err, docs);
+                    db.close();
+                    this.request = {};
+                });
+            }
+        });
+    },
+    update: function (callback) {
+        connectToMongo(this, function (err, collection, db, condition) {
+            if (err) {
+                callback(err);
+            } else {
+                var updateObj = {};
+                updateObj.$set = this.set;
+                updateObj.$currentDate = {lastModified: true};
+                collection.update(condition, updateObj, function (err, docs) {
+                    callback(err, docs);
+                    db.close();
+                    this.request = {};
+                    this.setData = {};
+                });
+            }
+        });
+    },
+    insert: function (newRecord, callback) {
+        connectToMongo(this, function (err, collection, db, condition) {
+            if (err) {
+                callback(err);
+            } else {
+                collection.insert(newRecord, function (err, docs) {
+                    callback(err, docs);
+                    db.close();
+                    this.request = {};
+                });
+            }
         });
     }
 };
